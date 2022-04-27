b4d6095ff0c9587db50e344d40d58edfb6966b05
Murasya <s2120644@s.tsukuba.ac.jp>
2022-04-27 22:15:46 +0900

    deploygateAPI追加

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,9 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    DeployGateSDK
+      .sharedInstance()
+      .launchApplication(withAuthor: "toten", key: "f758b64c485c868cc776347d1b05019842e34e96")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
